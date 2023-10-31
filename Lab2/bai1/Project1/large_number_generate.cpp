7ae63da7a434f275c6769558949e4db891680507
NGODAT0103 <21521935@gm.uit.edu.vn>
2023-10-31 21:54:59 +0700

    Lab1-bai1: thêm function trừ nhị phân

--- Lab2/bai1/Project1/large_number_generate.cpp
+++ Lab2/bai1/Project1/large_number_generate.cpp
@@ -27,10 +27,78 @@ bitset<128> large_number_generate() {
 }
 
 
+bool min_bits(bitset<128> a, bitset<128> b) {
+    for (int i = 127; i >= 0;i--)
+        if (a[i] == 1 && b[i] == 1)
+            continue;
+        else if (a[i] == 1 && b[i] == 0)
+            return false;
+        else
+            return true;
+
+}
+
+bitset<128> minus_bits(bitset<128> a, bitset <128> b) {
+    bitset<128> result;
+    result.reset();
+    bool borrow = false;
+    for (int i = 0; i < 128 ;i++) {
+
+
+        if (borrow) {
+            if (a[i] == 1 && b[i] == 1)
+                result.set(i);
+            else if (a[i] == 1 && b[i] == 0)
+            {
+                result.reset(i);
+                borrow = false;
+            }
+            else if (a[i] == 0 && b[i] == 1)
+                result.reset(i);
+            else if (a[i] == 0 && b[i] == 0)
+                result.set(i);
+
+            continue;
+        }
+
+
+
+        if (a[i] == 1 && b[i] == 1)
+            result.reset(i);
+        else if (a[i] == 1 && b[i] == 0)
+            result.set(i);
+        else if (a[i] == 0 && b[i] == 1) {
+            result.set(i);
+            borrow = true;
+        }
+        
+
+
+
+    }
+
+
+        return result;
+
+
+}
+
+bitset<128> gcd(bitset<128> a, bitset<128> b) {
+    bitset<128> result;
+    if (min_bits(a, b))
+       result =  minus_bits(b, a);
+    else result =  minus_bits(a, b); 
+    cout << result.to_string();
+
+    return result;
+    
+}
+
+
 int main() {
 
-    large_number_generate();
-    large_number_generate();
-
+    bitset<128> a =  large_number_generate();
+    bitset<128> b =  large_number_generate();
+    gcd(a, b);
     return 0;
 }

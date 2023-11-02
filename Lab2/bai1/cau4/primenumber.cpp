eef7a1b7c96760923455d211eb88e269eaeec758
NGODAT0103 <21521935@gm.uit.edu.vn>
2023-11-02 15:19:44 +0700

    4.1 done

--- Lab2/bai1/cau4/primenumber.cpp
+++ Lab2/bai1/cau4/primenumber.cpp
@@ -3,9 +3,40 @@ using namespace std;
 #include<bitset>
 #include<vector>
 #include<math.h>
+#include <random>
+#include <chrono>
 
-bitset<81> divisor_increment(bitset<81> current_bitset) {
-	for(int i = 80 ;i>=0 ;i--)
+
+
+
+
+
+bitset<95> large_number_generate() {
+	bitset<95>large_number_bits;
+
+	random_device dev;
+	mt19937 rng(dev());
+	std::uniform_int_distribution<std::mt19937::result_type> dist6(0, 1);
+
+	for (int i = 0; i < 89;i++)
+		large_number_bits.set(i);
+
+
+	for (int i = 89; i <95;i++)
+		if (dist6(rng) == 1)
+			large_number_bits.set(i);
+		else
+			continue;
+
+	return large_number_bits;
+}
+
+
+
+
+
+bitset<95> divisor_increment(bitset<95> current_bitset) {
+	for(int i = 94 ;i>=0 ;i--)
 		if (current_bitset[i] == 1) {
 
 			if (i == 0) {
@@ -112,11 +143,11 @@ vector<bool> minus_bits(vector<bool> a, vector<bool> b) {
 
 
 
-bool divide_bits(bitset<81> a, bitset<81> b) {
+bool divide_bits(bitset<95> a, bitset<95> b) {
 	vector<bool> the_dividend;
 	vector<bool> divisor;
 	vector<bool> result;
-	for (int i = 80;i >= 0;i--)
+	for (int i = 94;i >= 0;i--)
 		if (a[i] == 1) {
 			the_dividend = vector<bool>(i + 1);
 			for (int j = i; j >= 0;j--)
@@ -130,7 +161,7 @@ bool divide_bits(bitset<81> a, bitset<81> b) {
 			break;
 		}
 
-	for (int i = 80;i >= 0;i--)
+	for (int i = 94;i >= 0;i--)
 		if (b[i] == 1) {
 			divisor = vector<bool>(i + 1);
 			for (int j = i; j >= 0;j--)
@@ -143,10 +174,14 @@ bool divide_bits(bitset<81> a, bitset<81> b) {
 
 			break;
 		}
+
+
+	cout << "The dividend:" << endl;
 	print_vector(the_dividend);
+	cout << endl;
+
+	cout << "The divisor" << endl;
 	print_vector(divisor);
-
-
 
 
 
@@ -229,43 +264,62 @@ bool divide_bits(bitset<81> a, bitset<81> b) {
 
 
 int main() {
-	bitset<81> the_dividend = bitset<81>("01010111101");
-	bitset<81> divisor = bitset<81>("1");
+
+
+	auto start = std::chrono::high_resolution_clock::now();
+
+	bitset<95> the_dividend = large_number_generate();
+
+
+
+
+	//bitset<95> the_dividend = bitset<95>("01111000101011");
+
+	bitset<95> divisor = bitset<95>("1");
 
 	
-	//cout<< divide_bits(the_dividend, divisor);
 
-	//return 0 ;
 
 
 
 	int count = 1;
 
 
-	do {
+	while (1) {
+
+		if (count > 2 || (count == 2 && the_dividend != divisor)) {
+			count++;
+			cout << "So lan dang chia het: " << count << endl;
+
+			break;
+		}
 		divisor = divisor_increment(divisor);
 
 		if (divide_bits(the_dividend, divisor))
-		{
 			count += 1;
-			divide_bits(the_dividend, divisor);
-		}
-		cout << "divisor: " << divisor.to_string() << endl;
-	} while (the_dividend != divisor);
-
+		if (the_dividend == divisor)
+			break;
+		cout <<"So lan dang chia het: "<< count << endl;
+	}
 
 
 
 	cout << endl << endl;
+
+	cout << "Large number is :" << endl<< the_dividend.to_string() << endl;
 	if (count == 2)
 		cout << "La so nguyen to " << endl;
 	else
-		cout << "Khong la so nguyen to " << endl;
+		cout << "Khong la so nguyen to do count > 2 " << endl;
 
 
-	cout << "So lan chia het: " << count;
+
+
+
 	
-	
+	auto end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> elapsed = end - start;
+	std::cout << "Elapsed time: " << elapsed.count() << " seconds" << std::endl;
 	
 
 }
